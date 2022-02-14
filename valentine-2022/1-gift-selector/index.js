d1ea7dfd8094952a449ff807172d731eb7f6b0d6
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-02-14 22:22:34 +0100

    Functionality

--- valentine-2022/1-gift-selector/index.js
+++ valentine-2022/1-gift-selector/index.js
@@ -1,7 +1,16 @@
 document.getElementById("btn").addEventListener("click", calculate)
 let costEl = document.getElementById("cost-el")
+const foodSelect = document.getElementById("food-select")
+const transportSelect = document.getElementById("transport-select")
 
 function calculate() {
   // 1. Write the JavaScript to calculate the total cost of the selected options from the form.
   // 2. Display the total cost in the cost-el span.
+  const foodPrice = parseInt(foodSelect.value)
+  const transportPrice = parseInt(transportSelect.value)
+  const totalCost = foodPrice + transportPrice
+
+  if (foodPrice && transportPrice) {
+    costEl.textContent = totalCost
+  }
 }
