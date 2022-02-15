61f3841d61d16a6578b285b514fc37a3d200c624
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-02-15 17:52:12 +0100

    Add functionality

--- valentine-2022/2-greetings-challenge/index.js
+++ valentine-2022/2-greetings-challenge/index.js
@@ -1,5 +1,16 @@
 const greetingDisplay = document.getElementById("greeting-display")
+const form = document.getElementById("form")
+const recipientInput = document.getElementById("recipient-input")
+const greetingSelect = document.getElementById("greeting-select")
+const senderInput = document.getElementById("sender-input")
+
+form.addEventListener("input", writeGreeting)
 
 function writeGreeting() {
   // Write a function update the message in the greeting-display paragraph each time the form is updated.
+  greetingDisplay.innerText = `From: ${recipientInput.value}
+  To: ${senderInput.value}
+
+  ${greetingSelect.value}
+  `
 }
