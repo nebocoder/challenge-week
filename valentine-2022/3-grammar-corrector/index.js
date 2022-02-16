3b78456fed914b196dd3963604472d9a758a1f69
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-02-16 17:16:52 +0100

    Challenge setup

---
+++ valentine-2022/3-grammar-corrector/index.js
@@ -0,0 +1,7 @@
+document.getElementById("btn").addEventListener("click", correct)
+
+function correct() {
+  // Write the JavaScript to grab the message from the paragraph and correct its capitalisation.
+  // It should read "Valentine's"
+  // Render the corrected message to the DOM.
+}
