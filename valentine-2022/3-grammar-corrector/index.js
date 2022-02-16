b1f3a4fbcc4911fc7f5b4656fb3652d7da75e811
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-02-16 17:32:25 +0100

    Functionality

--- valentine-2022/3-grammar-corrector/index.js
+++ valentine-2022/3-grammar-corrector/index.js
@@ -1,7 +1,10 @@
+const text = document.getElementById("paragraph").textContent
 document.getElementById("btn").addEventListener("click", correct)
 
 function correct() {
   // Write the JavaScript to grab the message from the paragraph and correct its capitalisation.
   // It should read "Valentine's"
   // Render the corrected message to the DOM.
+  document.getElementById("paragraph").textContent =
+    text[0].toUpperCase() + text.slice(1).toLowerCase()
 }
