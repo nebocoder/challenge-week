9bd124553280137862b9e1bb0b780bcce689e5f0
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-02-18 14:50:17 +0100

    Stretch goal

--- valentine-2022/5-heart-customiser/index.js
+++ valentine-2022/5-heart-customiser/index.js
@@ -1,13 +1,15 @@
 const body = document.body
+const heart = document.getElementById("heart")
 document.getElementById("btn").addEventListener("click", changeColor)
 
 async function changeColor() {
   // 1. Call Scrimba's color API (https://apis.scrimba.com/hexcolors/) to retrieve one random color.
   // 2. Use that color to update the background color of the body.
   async function getColors() {
-    const response = await fetch("https://apis.scrimba.com/hexcolors/?count=1")
+    const response = await fetch("https://apis.scrimba.com/hexcolors/?count=2")
     const data = await response.json()
     body.style.backgroundColor = data.colors[0].value
+    heart.style.backgroundColor = data.colors[1].value
   }
 
   getColors()
