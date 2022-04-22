3b2c65f58e413f365010663f5adc4a5784a41748
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-04-22 19:41:27 +0200

    Challenge setup

---
+++ earth-day-2022/5-reusable-cup/index.js
@@ -0,0 +1,13 @@
+const coffeesPerDay = [3, 1, 2, 1, 3, 2, 10];
+const reusableCupDiscount = 0.5;
+const coffeePrice = 3;
+const dailySavings = document.getElementById('daily-savings');
+let savingsPerDayArr = [];
+
+// Task: Based on the number of coffees bought and the reusable cup discount, calculate and render each day's savings in the dailySavings element.
+
+// stretch goal 1️⃣: Show the total savings
+
+// stretch goal 2️⃣: Add an input to add more entries to coffeesPerDay
+
+// stretch goal 3️⃣: Improve the formatting of the numbers, e.g. $1.5 = $1.50
