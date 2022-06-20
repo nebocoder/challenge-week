102dc3918dc4fe90c1c43b88aed7c7430c5eb902
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-06-20 17:26:16 +0200

    Challenge setup

---
+++ space-2022/1-render-solar-system/index.js
@@ -0,0 +1,51 @@
+const solarSystem = document.getElementById('solarSystem');
+const planets = [
+  {
+    name: 'Mercury',
+    diameter: 4879,
+    color: 'grey',
+  },
+  {
+    name: 'Venus',
+    diameter: 12104,
+    color: 'yellow',
+  },
+  {
+    name: 'Earth',
+    diameter: 12742,
+    color: 'green',
+  },
+  {
+    name: 'Mars',
+    diameter: 6779,
+    color: 'pink',
+  },
+  {
+    name: 'Jupiter',
+    diameter: 139822,
+    color: 'yellow',
+  },
+  {
+    name: 'Saturn',
+    diameter: 116464,
+    color: 'orange',
+  },
+  {
+    name: 'Uranus',
+    diameter: 50724,
+    color: 'grey',
+  },
+  {
+    name: 'Neptune',
+    diameter: 49244,
+    color: 'blue',
+  },
+];
+
+// Task:
+// Write a function to render
+// the planets from the planets array using JavaScript.
+
+// Stretch goals:
+// - 1. Show planet facts on hover
+// - 2. Make one side of the planets dark (you can do this with one CSS property!)
