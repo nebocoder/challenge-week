7ac85d83d1d620cf6196ec7629e4e8740bc030f0
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-06-21 18:17:44 +0200

    Challenge setup

---
+++ space-2022/2-galaxy-array/index.js
@@ -0,0 +1,7 @@
+// Task:
+// Write a function to display a galaxy of stars (at least 100) in the DOM.
+// Note: This requires very little code!
+
+// Stretch goals:
+// - 1. Ensure that the stars always fill each line
+// - 2. Add different sizes of star
