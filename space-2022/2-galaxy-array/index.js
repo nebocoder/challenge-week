5a6d1ad10411add8d27920acec9402734a053532
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-06-21 18:26:26 +0200

    JS

--- space-2022/2-galaxy-array/index.js
+++ space-2022/2-galaxy-array/index.js
@@ -1,6 +1,15 @@
 // Task:
 // Write a function to display a galaxy of stars (at least 100) in the DOM.
 // Note: This requires very little code!
+const galaxy = document.getElementById('galaxy');
+
+function renderStars() {
+  for (let i = 0; i < 100; i++) {
+    galaxy.innerHTML += `<p>⭐</p>`;
+  }
+}
+
+renderStars();
 
 // Stretch goals:
 // - 1. Ensure that the stars always fill each line
