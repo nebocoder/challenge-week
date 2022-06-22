33910d6463294f9fac69307c7c926894e108aa45
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-06-22 20:13:07 +0200

    Lift off animation

--- space-2022/3-lift-off/index.js
+++ space-2022/3-lift-off/index.js
@@ -6,18 +6,20 @@ let timeRemaining = 60;
 // 1. Write the JavaScript to count down from 60 to 0 and display the remaining time
 // 2. When the countdown reaches 0, launch the rocket with an animation
 
-function timer(time) {
-  setInterval(() => {
+function countdown(time) {
+  let timer = setInterval(() => {
     if (time > 0) {
       time--;
       countdownDisplay.textContent = `${time}`;
     } else {
       countdownDisplay.textContent = 'LIFT OFF!';
+      rocket.classList.add('launched');
+      clearInterval(timer);
     }
   }, 1000);
 }
 
-document.onload(timer(timeRemaining));
+countdown(timeRemaining);
 
 // Stretch goals:
 // 1) Add fire under the rocket.
