d83b54c26706beadae438092f200ac677a9b3983
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-06-22 19:57:45 +0200

    Timer function

--- space-2022/3-lift-off/index.js
+++ space-2022/3-lift-off/index.js
@@ -6,6 +6,19 @@ let timeRemaining = 60;
 // 1. Write the JavaScript to count down from 60 to 0 and display the remaining time
 // 2. When the countdown reaches 0, launch the rocket with an animation
 
+function timer(time) {
+  setInterval(() => {
+    if (time > 0) {
+      time--;
+      countdownDisplay.textContent = `${time}`;
+    } else {
+      countdownDisplay.textContent = 'LIFT OFF!';
+    }
+  }, 1000);
+}
+
+document.onload(timer(timeRemaining));
+
 // Stretch goals:
 // 1) Add fire under the rocket.
 // 2) Randomly don’t launch the rocket when the timer reaches 0.
