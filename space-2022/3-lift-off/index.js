e48ea18d8ee585cbcdea30908fadc3aa16f3054c
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-06-22 19:51:17 +0200

    Challenge setup

---
+++ space-2022/3-lift-off/index.js
@@ -0,0 +1,11 @@
+const countdownDisplay = document.getElementById('countdown-display');
+const rocket = document.getElementById('rocket');
+let timeRemaining = 60;
+
+// Task:
+// 1. Write the JavaScript to count down from 60 to 0 and display the remaining time
+// 2. When the countdown reaches 0, launch the rocket with an animation
+
+// Stretch goals:
+// 1) Add fire under the rocket.
+// 2) Randomly don’t launch the rocket when the timer reaches 0.
