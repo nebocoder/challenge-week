954b384e7b9a87a5ffa38b9c279bead7da52937e
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-06-23 17:01:58 +0200

    Challenge setup

---
+++ space-2022/4-total-moons/index.js
@@ -0,0 +1,43 @@
+const solarSystem = document.getElementById('solarSystem');
+const planetsObj = [
+  {
+    name: 'Mercury',
+    moons: 0,
+  },
+  {
+    name: 'Venus',
+    moons: 0,
+  },
+  {
+    name: 'Earth',
+    moons: 1,
+  },
+  {
+    name: 'Mars',
+    moons: 2,
+  },
+  {
+    name: 'Jupiter',
+    moons: 67,
+  },
+  {
+    name: 'Saturn',
+    moons: 62,
+  },
+  {
+    name: 'Uranus',
+    moons: 27,
+  },
+  {
+    name: 'Neptune',
+    moons: 14,
+  },
+];
+
+// Task:
+// 1. Display each planet’s name and number of moons in the DOM
+// 2. Calculate and display the total number of moons in the solar system below
+
+// Stretch goals:
+// - 1: Make the numbers count up on page load
+// - 2: Make the planet names bold.
